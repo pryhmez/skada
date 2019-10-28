fcd745bc9e1594ce91daae7ae627249fd3230d64
jerrybuks <profkiti@gmail.com>
2019-10-28 10:33:04 +0100

    setup error handling and validation

--- backend/middleWares/userValidation.js
+++ backend/middleWares/userValidation.js
@@ -1,28 +1,14 @@
 const { check } = require('express-validator');
-const userValidation = (type) => {
-  let validationRes = [];
-    switch (type) {
-        case "create user":
-            
-          valiationRes =  [
-                    check('email', 'Please include a valid email').isEmail(),
-                    check(
-                      'password',
-                      'Please enter a password with 8 or more characters'
-                    ).isLength({ min: 8 }),
-                    check('phone', 'Please enter a valid phone number').if(value => (/^0[7-9][0-1]\d{8}$/g).test(value)),
-                    check('businessphone', 'Please enter a valid phone number').if(value => (/^0[7-9][0-1]\d{8}$/g).test(value))
-                  ]
-            break;
-    
-        default:
-            break;
-    }
-    console.log(validationRes)
-    return validationRes;
-}
 
-module.exports = userValidation;
+const userSignUpValidation = [
+  check('firstname', 'Please inut your first name').not().isEmpty(),
+	check('lastname', 'Please inut your last  name').not().isEmpty(),  
+	check('email', 'Please include a valid email').isEmail(),
+	check('password', 'Please enter a password with 8 or more characters').isLength({ min: 8 }),
+  check('phone', 'Please enter a valid phone number').custom((value) => (/^0[7-9][0-1]\d{8}$/g).test(value)),
+	check('businessname', 'Please input your name').not().isEmpty(),  
+  check('businessphone', 'Please enter a valid phone number').custom((value) => (/^0[7-9][0-1]\d{8}$/g).test(value)),
+	check('businesscategory', 'Please Select a  business category').not().isEmpty(),    
+];
 
-
-
+module.exports = { userSignUpValidation };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 const userController = require("../controllers/userController");
-const userValidation   =  require("../middleWares/userValidation")
+const {  userSignUpValidation }  =  require("../middleWares/userValidation");
 
 module.exports = function() {
    
     var userCtl = new userController();
     
-    router.post("/signUp",  userValidation("create user"), userCtl.signUp);
+    router.post("/signUp", userSignUpValidation , userCtl.signUp);
     router.post("/login", userCtl.login);
     
     return router;
