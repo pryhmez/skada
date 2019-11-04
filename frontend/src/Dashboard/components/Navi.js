46f6e4b47a6040cdee4a432b2edc1c4550c96a49
FelixFaust2016 <victoriheukwmere@gmail.com>
2019-11-04 14:29:37 -0800

    third commit

--- frontend/src/Dashboard/components/Navi.js
+++ frontend/src/Dashboard/components/Navi.js
@@ -3,7 +3,12 @@ import {ProductConsumer} from "../../Context/Context"
 import Logo from "../../components/Logo";
 import Button from "./DashButton"
 import "../css/Navi.css";
-import {NavLink} from "react-router-dom"
+import {NavLink} from "react-router-dom";
+import One from "../icons/1.svg"
+import Two from "../icons/2.png"
+import Three from "../icons/3.png"
+import Four from "../icons/4.png"
+import Five from "../icons/5.png"
 
 class Navi extends Component {
     state = { 
@@ -12,28 +17,33 @@ class Navi extends Component {
             {
                 name:'Dashboard',
                 route:'/dashboard',
-                active:'active'
+                active:'active',
+                icon:One
             },
             {
                 name:'Delivery Status',
                 route:'/schedule',
-                active:'active1'
+                active:'active1',
+                icon:Two
             },
             {
                 name:'Track Goods',
                 route:'/tracking',
-                active:'active2'
+                active:'active2',
+                icon:Three
 
             },
             {
                 name:'Manage',
                 route:'',
-                active:'active3'
+                active:'active3',
+                icon:Four
             },
             {
                 name:'Logout',
                 route:'',
-                active:'active4'
+                active:'active4',
+                icon:Five
             }
         ]
     }
@@ -52,7 +62,7 @@ class Navi extends Component {
                 <header style={{marginLeft:'80px'}} className="header">Hey <side>Hunger Clinic</side></header>
                 {this.state.route.map(data => <div style={{marginTop:"40px"}}>
                     <aside style={{paddingLeft:'80px'}}><NavLink style={{color:'white',textDecoration:'none',width:'100%'}}
-                     exact to={data.route} className="navline" activeClassName={data.active}>{data.name}</NavLink></aside>
+                     exact to={data.route} className="navline" activeClassName={data.active}><img style={{marginRight:'15px',marginTop:'5px'}} src={data.icon}></img>{data.name}</NavLink></aside>
                 </div>)}
                 <div style={{marginTop:'40px',paddingLeft:'80px'}}><Button/></div>
             </div>)}}
