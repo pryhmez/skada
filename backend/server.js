25f06bda265e23d003668c29303db49ecbed736c
PRIME <edmundisaac16@gmail.com>
2019-11-07 18:05:07 +0100

    orio

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var { keySecret, keyPublishable } = require("./config/stripeConfig")
+var { keySecret, keyPublishable } = require("./config/stripeConfig");
 var app = express();
 var http = require('http');
 var cors = require('cors');
@@ -19,8 +19,8 @@ var corsOptions = {
 	origin: ['http://localhost:3000', 'https://skada.netlify.com/'],
 	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
-
 dotenv.config({ path: './config/config.env' });
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
@@ -28,40 +28,32 @@ app.use('/uploads', express.static('uploads'));
 
 app.use(express.urlencoded({ extended: false }))
 
+const server = http.createServer(app);
 app.set('views', path.join(__dirname, 'views'));
 // set view engine as pug
 app.set('view engine', 'pug');
-// to open the payment page on base url
-app.get("/", ((req, res) => {
-	res.render("index", { keyPublishable });
-}));
 
-app.post("/charge", function (req, res) {
+// app.post("/charge", function (req, res) {
 
-	let amount = 10 * 100;
+// 	let amount = 10 * 100;
 
-	// create a customer
-	stripe.customers.create({
-		email: req.body.stripeEmail, // customer email
-		source: req.body.stripeToken // token for the card
-	})
-		.then(customer =>
-			stripe.charges.create({ // charge the customer
-				amount,
-				description: "Sample Charge",
-				currency: "usd",
-				customer: customer.id
-			}))
-		.then(charge => res.render("charge")); // render the payment successful alter page after payment
+// 	// create a customer
+// 	stripe.customers.create({
+// 		email: req.body.stripeEmail, // customer email
+// 		source: req.body.stripeToken // token for the card
+// 	})
+// 		.then(customer =>
+// 			stripe.charges.create({ // charge the customer
+// 				amount,
+// 				description: "Sample Charge",
+// 				currency: "usd",
+// 				customer: customer.id
+// 			}))
+// 		.then(charge => res.render("charge")); // render the payment successful alter page after payment
 
-});
+// });
 
-
-const server = http.createServer(app);
-
-// app.all("*", (request, response) => {response.send("k,rhherth")})
-
-app.use('/api', appRoutes(Router));
+app.use('/api', appRoutes(Router, app));
 app.all('*', (req, res, next) => {
 	next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
